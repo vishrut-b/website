236c4cf6197957babd839f073d4d1bb674582ae0
Vishrut Bezbarua <vishrutbezbarua@gmail.com>
2025-06-18 12:06:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const readMoreBtn = document.getElementById('readMoreBtn');
+if (readMoreBtn) {
+  const moreContent = document.getElementById('intro-more');
+  readMoreBtn.addEventListener('click', () => {
+    const visible = moreContent.style.display === 'block';
+    moreContent.style.display = visible ? 'none' : 'block';
+    readMoreBtn.textContent = visible ? 'Read More' : 'Read Less';
+  });
+}
